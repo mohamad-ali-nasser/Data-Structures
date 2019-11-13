6e5c2034fc15850eb0960889210db09039b981f4
cartman12 <mhd.ali.nasser@gmail.com>
2019-11-13 02:34:11 +0200

    Queue stack assignment

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('../doubly_linked_list')
+# sys.path.append("C:\Users\mhdal\github\Data-Structures\doubly_linked_list")
 from doubly_linked_list import DoublyLinkedList
 
 
@@ -7,13 +7,22 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # self.value = None
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        self.value = value
+        self.storage.add_to_head(self.value)
+        self.size += 1
 
     def dequeue(self):
-        pass
+        if self.size == 0:
+            self.size = 0
+        else:
+            value = self.storage.tail.value
+            self.storage.remove_from_tail()
+            self.size -= 1
+            return value
 
     def len(self):
-        pass
+        return self.size
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,18 +1,26 @@
 from doubly_linked_list import DoublyLinkedList
 import sys
-sys.path.append('../doubly_linked_list')
+# sys.path.append('../doubly_linked_list')
 
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        self.value = value
+        self.storage.add_to_tail(self.value)
+        self.size += 1
 
     def pop(self):
-        pass
+        if self.size == 0:
+            self.size = 0
+        else:
+            v = self.storage.tail.value
+            self.storage.remove_from_tail()
+            self.size -= 1
+            return v
 
     def len(self):
-        pass
+        return self.size
